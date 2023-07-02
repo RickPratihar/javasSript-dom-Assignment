c9b6d8aabd2ba8eee47e54fb8eb972619a18793d
Rick Pratihar <103872207+rick7890@users.noreply.github.com>
2023-07-02 17:42:48 +0530

    Add files via upload

---
+++ Event Listener Project/mouse out/script.js
@@ -0,0 +1,6 @@
+const boxElement = document.getElementById("box")
+boxElement.addEventListener("mouseout", function(){
+    alert(
+         "you entered in Restricted Area 🙅 (mouse out event is working properly)"
+         )
+})
